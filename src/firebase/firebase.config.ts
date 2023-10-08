9badf8d23f3d379eaa6167d53d3d0d1394e6f8ec
macbook kal <arnaud.pantenier@gmail.com>
2023-10-08 16:37:18 +0200

    Version fonctionnelle avec variables env

--- src/firebase/firebase.config.ts
+++ src/firebase/firebase.config.ts
@@ -6,24 +6,17 @@ import { getAuth } from 'firebase/auth';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
 const firebaseConfig = {
-  apiKey: 'AIzaSyBH0gPS0ZwGr4r19rPG6KmJtbGbCxsQirE',
-  authDomain: 'rack-a-pinard.firebaseapp.com',
-  projectId: 'rack-a-pinard',
-  storageBucket: 'rack-a-pinard.appspot.com',
-  messagingSenderId: '141140659387',
-  appId: '1:141140659387:web:7359bf8e699f2e19652d9e',
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 
-// const firebaseConfig = {
-//   apiKey: process.env.REACT_APP_API_KEY,
-//   authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-//   projectId: process.env.REACT_APP_PROJECT_ID,
-//   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
-//   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
-//   appId: process.env.REACT_APP_APP_ID,
-// };
--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,32 @@
-import { defineConfig } from 'vitest/config'
+// import { defineConfig } from 'vitest/config'
+// import react from '@vitejs/plugin-react'
+
+// // https://vitejs.dev/config/  
+// export default defineConfig({
+  
+//   plugins: [react()],
+//   test: {
+//     globals: true,
+//     environment: 'jsdom',
+//     setupFiles: ['./vitest.setup.ts']
+//   }
+// })
+
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/  
-export default defineConfig({
-  plugins: [react()],
-  test: {
-    globals: true,
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts']
+// https://vitejs.dev/config/
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '');
+  return {
+    define: {
+      'process.env.SOME_KEY': JSON.stringify(env.SOME_KEY)
+    },
+    plugins: [react()],
+    test: {
+          globals: true,
+          environment: 'jsdom',
+          setupFiles: ['./vitest.setup.ts']
+        }
   }
 })
