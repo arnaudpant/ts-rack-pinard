ad5c8ce69f562cfa418897c6b9c0e69aeec9e54f
macbook kal <arnaud.pantenier@gmail.com>
2023-10-23 16:25:14 +0200

    Ajout redirection page inscription

--- src/ui/pages/Inscription.tsx
+++ src/ui/pages/Inscription.tsx
@@ -1,17 +1,32 @@
-import { Link } from "react-router-dom";
+import { Link, Navigate } from "react-router-dom";
 import BoxInscription from "../modules/authentification/inscription/BoxInscription";
+import { useAuth } from "../../context/AuthUserContext";
 
 
 const Inscription = () => {
+    const { authUser } = useAuth()
+
     return (
-        <div className="flex flex-col items-center">
-            <h2 className="text-center text-xl py-4 px-1">Créez un compte pour pouvoir gérer vos casiers à bouteilles</h2>
-            <div>
-                <img src="/wine-bottle.svg" alt="bouteille de vin" className="h-20 w-20" />
-            </div>
-            <BoxInscription />
-            <p className="text-sm text-center pb-4">En t'inscrivant, tu acceptes les <Link to="/#" className="text-vin">conditions d'utilisation</Link>  et la <Link to="/#" className="text-vin">politique de confidentialité</Link></p>
-        </div>
+        <>
+            {
+                authUser !== null ? (
+                    <Navigate to="/container-racks" replace={true} />
+                )
+                    :
+                    (
+                        <div className="flex flex-col items-center">
+                            <h2 className="text-center text-xl py-4 px-1">Créez un compte pour pouvoir gérer vos casiers à bouteilles</h2>
+                            <div>
+                                <img src="/wine-bottle.svg" alt="bouteille de vin" className="h-20 w-20" />
+                            </div>
+                            <BoxInscription />
+                            <p className="text-sm text-center pb-4">En t'inscrivant, tu acceptes les <Link to="/#" className="text-vin">conditions d'utilisation</Link>  et la <Link to="/#" className="text-vin">politique de confidentialité</Link></p>
+                        </div>
+                    )
+            }
+        </>
+
+
     );
 };
 
