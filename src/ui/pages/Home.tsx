62909daeb0704185f061dd4e73830d03c20cfd01
macbook kal <arnaud.pantenier@gmail.com>
2023-11-03 13:59:14 +0100

    redirection accueil

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,9 +1,9 @@
 /** CONTEXT */
 import { useAuth } from "../../context/AuthUserContext";
 /** ROOTER */
-import { Navigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 /** COMPONENTS */
-import Accueil from "../modules/accueil/Accueil";
+
 
 const Home = () => {
 
@@ -11,9 +11,27 @@ const Home = () => {
 
     return (
         <>
-            {
-                authUser !== null ? (<Navigate to="/container-racks" replace={true} />) : (<Accueil /> )
-            }
+
+
+            <div className="flex flex-col">
+                <h2 className="text-center text-xl italic py-4 px-1">Visualisez et gèrez vos casiers à bouteilles !</h2>
+                <div className="h-80 md:h-96 w-full flex justify-center">
+                    <img src="/vine.svg" alt="" className="h-80 w-80 md:h-full" />
+                </div>
+
+                {
+                    authUser !== null ? (
+                        <div className="flex justify-center gap-4">
+                            <Link to='/container-racks' className="px-5 py-3 bg-vin text-fond rounded-lg shadow-sm" >MES RACKS</Link>
+                        </div>
+                    ) : (
+                        <div className="flex justify-center gap-4">
+                            <Link to='/inscription' className="px-3 py-1 bg-vin text-fond rounded-lg shadow-sm" >INSCRIPTION</Link>
+                            <Link to='/connexion' className="px-3 py-1 bg-vin text-fond rounded-lg shadow-sm" >CONNEXION</Link>
+                        </div>
+                    )
+                }
+            </div>
         </>
     );
 };
