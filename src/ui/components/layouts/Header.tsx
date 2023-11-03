225f873ae4ed5776d2ad3f5ba4c629971dae329a
macbook kal <arnaud.pantenier@gmail.com>
2023-11-03 16:51:45 +0100

    COR Bug

--- src/ui/components/layouts/Header.tsx
+++ src/ui/components/layouts/Header.tsx
@@ -8,15 +8,15 @@ const Header: React.FC = () => {
 
     let scrAvatar: string = ""
 
-    if(authUser !== null) {
-        scrAvatar = authUser?.userDocument.photoURL
+    if(authUser?.userDocument.photoURL) {
+        scrAvatar = authUser.userDocument.photoURL
     }
 
     return (
         <header className="relative flex items-center justify-center h-16 bg-vin text-fond ">
             <h1 className="text-2xl md:text-4xl py-2">RACKS A PINARD 🍾🍷</h1>
             
-            <div className={clsx(!authUser.userDocument.onBoardingisCompleted && `hidden`, 'absolute right-4 cursor-pointer w-14 h-14')}>
+            <div className={clsx(!authUser?.userDocument.onBoardingisCompleted && `hidden`, 'absolute right-4 cursor-pointer w-14 h-14')}>
                 {
                     scrAvatar == "" ? (<Avatar src="/avatar-default.png" width="14" height="14"/>) : (<Avatar src={scrAvatar} width="14" height="14"/>)                   
                 }
