a127308c3e50cb0175975b178ec8f7e528347780
macbook kal <arnaud.pantenier@gmail.com>
2023-11-06 11:34:52 +0100

    Modif css rackview

--- src/ui/modules/racks/RacksView.tsx
+++ src/ui/modules/racks/RacksView.tsx
@@ -2,8 +2,8 @@
 
 const RacksView = () => {
     return (
-        <div>
-            Rack 1
+        <div className="flex justify-center items-center">
+            <h1>See you soon ...</h1>
         </div>
     );
 };
