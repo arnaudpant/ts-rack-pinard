1368c1661fd66b7eb2fe27af86fec75a6858fddb
macbook kal <arnaud.pantenier@gmail.com>
2023-12-18 23:07:44 +0100

    modif colums rendu

--- src/ui/components/rack/DemoView.tsx
+++ src/ui/components/rack/DemoView.tsx
@@ -1,23 +1,33 @@
 import clsx from "clsx";
 import { Bottle, Rack } from "../../../types/RacksTypes";
 import BottlePinard from "./BottlePinard";
+import { useEffect, useState } from "react";
 
 type Props = {
     dataDemoRacks: Rack
 }
 
 const DemoView = ({ dataDemoRacks }: Props) => {
+    const [numberOfColums, setNumberOfColums] = useState(0)
+    useEffect(() => {
+        setNumberOfColums(dataDemoRacks.columns)
+    },[])
+
     return (
         <div>
             <h1 className="text-3xl text-center py-4">Bienvenue sur le rack de {`${dataDemoRacks.rackName}`}</h1>
             <div className="container mx-auto flex justify-center">
-                <div className={clsx("m-4 bg-gris_fonce p-2 grid gap-2", `grid-cols-${dataDemoRacks.columns}`)}>
-                    {
-                        dataDemoRacks.bottles.map((bottle: Bottle, index) => (
-                            <BottlePinard bottle={bottle} key={index} />
-                        ))
-                    }
-                </div>
+                {
+                    numberOfColums !== 0 && (
+                        <div className={clsx(`m-4 bg-gris_fonce p-2 grid gap-2 grid-cols-${numberOfColums}`)}>
+                            {
+                                dataDemoRacks.bottles.map((bottle: Bottle, index) => (
+                                    <BottlePinard bottle={bottle} key={index} />
+                                ))
+                            }
+                        </div> 
+                    )
+                }
 
             </div>
         </div>
--- src/ui/pages/Demo.tsx
+++ src/ui/pages/Demo.tsx
@@ -32,8 +32,6 @@ const Demo = () => {
 
     }, []);
 
-    console.log(dataDemoRacks)
-
 
     return (
         <>
