3721b64de883b71d563710f0ba54fcbeb5736f33
macbook kal <arnaud.pantenier@gmail.com>
2023-12-20 22:42:45 +0100

    commit apres merge

---
+++ src/types/Forms.ts
@@ -0,0 +1,34 @@
+export interface FormsType {
+    errors: any,
+    control?: any,
+    register: any,
+    handleSubmit: any,
+    onSubmit: any,
+    isLoading: boolean
+}
+
+export interface RegisterFormType {
+    email: string,
+    password: string
+}
+export interface ConnexionFormType {
+    email: string,
+    password: string
+}
+
+export interface ForgetFormType {
+    email: string,
+}
+
+export interface OnboardingProfileFormType {
+    login: string,
+}
+
+export interface userProfilFormtype {
+    errors: any,
+    control?: any,
+    register: any,
+    handleSubmit: any,
+    onSubmit: any,
+    isLoading: boolean
+}
---
+++ src/types/User.ts
@@ -0,0 +1,28 @@
+/** FIREBASE */
+import { Timestamp } from "firebase/firestore"
+import { Bottle, Rack } from "./RacksTypes"
+
+// Données primaires de Firebase
+export interface UserInterface {
+    // Données primaires
+    uid: string,
+    email: string | null,
+    displayName: string | null,
+    emailVerified: boolean,
+    photoURL: string | null,
+    // Données de la collection de la base de donnée
+    userDocument?: UserDocument 
+}
+
+export interface UserDocument {
+    photoURL: string
+    uid: string,
+    email: string,
+    creation_date: Timestamp,
+    onBoardingisCompleted: boolean,
+    login: string,
+    racks: Rack[],
+    bottlesFavoris: Bottle[],
+    bottlesDrink: Bottle[]
+    // Champs Ajouter un champ ds Firestore
+}
