515c1929a9ae495257130236350e14d86098ab65
macbook kal <arnaud.pantenier@gmail.com>
2023-12-26 22:18:01 +0100

    Ajout view des racks

--- src/ui/modules/racks/RacksView.tsx
+++ src/ui/modules/racks/RacksView.tsx
@@ -1,34 +1,47 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useAuth } from "../../../context/AuthUserContext";
 import AddRackModal from "./modal/AddRackModal";
-
+import { Rack } from "@/types/RacksTypes";
+import { ShieldPlus } from "lucide-react";
+import RackSoloView from "./modal/RackSoloView";
 
 const RacksView = () => {
-
-    const { authUser } = useAuth()
-    const [modalShow, setModalShow] = useState(false)
+    const { authUser } = useAuth();
+    const [modalShow, setModalShow] = useState(false);
+    const [listOfRacks, setListOfRacks] = useState<Rack[] | []>([]);
     const handleClick = () => {
-        setModalShow(v => !v)
-    }
+        setModalShow((v) => !v);
+    };
 
+    useEffect(() => {
+        authUser.userDocument.racks.length > 0 &&
+            setListOfRacks(authUser.userDocument.racks);
+    }, []);
+
+    //console.log(listOfRacks);
     return (
         <div className="flex justify-center items-center">
-            {
-                authUser.userDocument.racks.length > 0 &&
-                (
-                    <div className="">
-                        <h2>RackView</h2>
-                        <div>
-                        <button className="h-10 w-10 rounded-full bg-vin text-3xl text-fond text-center" onClick={handleClick} >+</button>
-                        </div>
+            {authUser.userDocument.racks.length > 0 && (
+                <div className="">
 
+                    {
+                        listOfRacks.map((rack) => (
+                            <RackSoloView rack={rack} key={rack.idrack} />
+                        ))
+                    }
+
+                    <div className="absolute bottom-4">
+                        <button
+                            className="flex justify-center items-center h-20 w-20 rounded-full bg-vin text-3xl text-fond text-center shadow-sm"
+                            onClick={handleClick}
+                        >
+                            <ShieldPlus className="h-10 w-10" />
+                        </button>
                     </div>
-                )
-            }
-            {
-                modalShow && <AddRackModal handleClick={handleClick} />
-            }
+                </div>
+            )}
 
+            {modalShow && <AddRackModal handleClick={handleClick} />}
         </div>
     );
 };
---
+++ src/ui/modules/racks/modal/RackSoloView.tsx
@@ -0,0 +1,77 @@
+import { Bottle, Rack } from "@/types/RacksTypes";
+import BottlePinard from "../../../../ui/components/rack/bottles/BottlePinard";
+import { useEffect, useState } from "react";
+
+type Props = {
+    rack: Rack;
+};
+
+const RackSoloView = ({ rack }: Props) => {
+    const [classGrid, setClassGrid] = useState<string>("")
+
+    useEffect(()=>{
+
+        switch (rack.columns) {
+            case 1:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-1 gap-1")
+                break;
+    
+            case 2:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-2 gap-1")
+                break;
+    
+            case 3:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-3 gap-1")
+                break;
+    
+            case 4:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-4 gap-1")
+                break;
+    
+            case 5:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-5 gap-1")
+                break;
+    
+            case 6:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-6 gap-1")
+                break;
+    
+            case 7:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-7 gap-1")
+                break;
+    
+            case 8:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-8 gap-1")
+                break;
+    
+            case 9:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-9 gap-1")
+                break;
+    
+            case 10:
+                setClassGrid("m-4 bg-gris_fonce p-2 grid grid-cols-10 gap-1")
+                break;
+        }
+
+    },[rack])
+
+
+    return (
+        <div className="flex flex-col items-center">
+            <h1 className="text-3xl text-center py-4">
+                Rack: {`${rack.rackName}`}
+            </h1>
+            <div className="container mx-auto flex justify-center">
+                {rack && classGrid !== "" && (
+                    <div className={`${classGrid}`}>
+                        {rack.bottles.map((bottle: Bottle) => (
+                            <BottlePinard bottle={bottle} key={bottle.id} />
+                        ))}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default RackSoloView;
