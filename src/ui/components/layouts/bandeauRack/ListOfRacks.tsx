e0236537a1dde9787f2b852e0e2c1ede1fc095cb
macbook kal <arnaud.pantenier@gmail.com>
2024-10-10 22:09:29 +0200

    REFAC Suppression useEffect dans ListOfRacks

--- src/ui/components/layouts/bandeauRack/ListOfRacks.tsx
+++ src/ui/components/layouts/bandeauRack/ListOfRacks.tsx
@@ -1,32 +1,31 @@
-import { useEffect, useState } from "react";
 import ButtonRackSelected from "./ButtonRackSelected";
-import { Rack } from "@/types/RacksTypes";
 import { useAuth } from "../../../../context/AuthUserContext";
 
 const ListOfRacks = () => {
     const { authUser } = useAuth();
-    const [listOfRacks, setListOfRacks] = useState<Rack[] | []>([]);
+    const listOfRacks = authUser?.userDocument.racks || [];
 
-    useEffect(() => {
-        authUser?.userDocument.racks.length > 0 &&
-            setListOfRacks(authUser.userDocument.racks);
-    }, [authUser]);
+    if (!authUser) {
+        return null; 
+    }
+
+    if (listOfRacks.length === 0) {
+        return <p>Vous n'avez pas encore de racks.</p>;
+    }
 
     return (
-        <>
-            {listOfRacks.length > 0 && authUser && (
-                <div className="flex flex-col items-center pt-5">
-                    <h2 className="text-xl text-vin800 text-center pb-2">
-                        Liste de vos racks:
-                    </h2>
-                    <div className="flex justify-center items-center flex-wrap gap-2 py-2 px-4">
-                        {listOfRacks.map((rack) => (
-                            <ButtonRackSelected rack={rack} key={rack.idrack} />
-                        )).sort()}
-                    </div>
-                </div>
-            )}
-        </>
+        <div className="flex flex-col items-center pt-5">
+            <h2 className="text-xl text-vin800 text-center pb-2">
+                Liste de vos racks:
+            </h2>
+            <div className="flex justify-center items-center flex-wrap gap-2 py-2 px-4">
+                {listOfRacks
+                    .map((rack) => (
+                        <ButtonRackSelected rack={rack} key={rack.idrack} />
+                    ))
+                    .sort()}
+            </div>
+        </div>
     );
 };
 
