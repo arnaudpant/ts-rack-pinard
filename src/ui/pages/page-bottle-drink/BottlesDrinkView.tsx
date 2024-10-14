b425c504d139dee5a3576a98de2dda02d86c5e60
macbook kal <arnaud.pantenier@gmail.com>
2024-10-14 22:05:55 +0200

    REFAC BottlesDrinkView

--- src/ui/pages/page-bottle-drink/BottlesDrinkView.tsx
+++ src/ui/pages/page-bottle-drink/BottlesDrinkView.tsx
@@ -12,58 +12,70 @@ import { dateFormater } from "../../../utils/utils";
 
 type Props = {
     bottlesDrink: Bottle[];
-    handleDelete: (id: string) => any;
+    handleDelete: (id: string) => void;
 };
 
-const BottlesDrinkView = ({ bottlesDrink, handleDelete }: Props) => {
+const tableHeaders = [
+    "Bouteilles",
+    "Type de vin",
+    "Appellation",
+    "Exploitation",
+    "Date achat",
+    "Prix",
+    "Supprimer",
+];
+
+const BottlesDrinkView: React.FC<Props> = ({ bottlesDrink, handleDelete }) => {
+    const renderTableHeader = () => (
+        <TableHeader>
+            <TableRow>
+                {tableHeaders.map((header, index) => (
+                    <TableHead key={index}>{header}</TableHead>
+                ))}
+            </TableRow>
+        </TableHeader>
+    );
+
+    const renderTableBody = () => (
+        <TableBody>
+            {bottlesDrink.map((bottle: Bottle) => (
+                <TableRow key={bottle.id}>
+                    <TableCell className="font-medium">{bottle.type}</TableCell>
+                    <TableCell>{bottle.couleur}</TableCell>
+                    <TableCell>{bottle.appellation}</TableCell>
+                    <TableCell>{bottle.exploitation}</TableCell>
+                    <TableCell>{dateFormater(bottle.achat)}</TableCell>
+                    <TableCell>{bottle.prix}€</TableCell>
+                    <TableCell
+                        className="cursor-pointer"
+                        onClick={() => handleDelete(bottle.id)}
+                        data-testid="supprimer"
+                    >
+                        <Trash2 className="text-vin" />
+                    </TableCell>
+                </TableRow>
+            ))}
+        </TableBody>
+    );
 
     return (
         <div className="container mx-auto p-4 min-h-[calc(100vh-234px)]">
             <h2 className="text-2xl pt-5 pb-2 text-center text-vin">
                 Liste des bouteilles consommées
             </h2>
-                {bottlesDrink.length > 0 ? (
-            <Table>
-                <TableHeader>
-                    <TableRow>
-                        <TableHead>Bouteilles</TableHead>
-                        <TableHead>Type de vin</TableHead>
-                        <TableHead>Appellation</TableHead>
-                        <TableHead>Exploitation</TableHead>
-                        <TableHead>Date achat</TableHead>
-                        <TableHead>Prix</TableHead>
-                        <TableHead>Supprimer</TableHead>
-                    </TableRow>
-                </TableHeader>
-                    <TableBody>
-                        {bottlesDrink.map((bottle: Bottle) => (
-                                <TableRow key={bottle.id}>
-                                    <TableCell className="font-medium">
-                                        {bottle.type}
-                                    </TableCell>
-                                    <TableCell>{bottle.couleur}</TableCell>
-                                    <TableCell>{bottle.appellation}</TableCell>
-                                    <TableCell>{bottle.exploitation}</TableCell>
-                                    <TableCell>
-                                        {dateFormater(bottle.achat)}
-                                    </TableCell>
-                                    <TableCell>{bottle.prix}€</TableCell>
-                                    <TableCell
-                                        className="cursor-pointer"
-                                        onClick={() => handleDelete(bottle.id)}
-                                        data-testid="supprimer"
-                                    >
-                                        <Trash2 className="text-vin" />
-                                    </TableCell>
-                                </TableRow>
-                        ))}
-                    </TableBody>
-            </Table>
-                ) : (
-                    <div className="text-center text-xl pt-10">Aucunes bouteilles consommées pour le moment</div>
-                )}
+            {bottlesDrink.length > 0 ? (
+                <Table>
+                    {renderTableHeader()}
+                    {renderTableBody()}
+                </Table>
+            ) : (
+                <div className="text-center text-xl pt-10">
+                    Aucunes bouteilles consommées pour le moment
+                </div>
+            )}
         </div>
     );
 };
+
 
 export default BottlesDrinkView;
