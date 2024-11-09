1b91b24a55da5effac3d783b7e3af98c475fc9af
macbook kal <arnaud.pantenier@gmail.com>
2024-11-09 11:11:35 +0100

    TEST Footer

---
+++ src/__tests__/Footer.test.tsx
@@ -0,0 +1,29 @@
+//import { it, expect, describe, beforeEach } from "vitest";
+import { render, screen, waitFor } from "@testing-library/react";
+import { AuthUserProvider } from "../context/AuthUserContext";
+import { BrowserRouter } from "react-router-dom";
+import Footer from "../ui/components/layouts/footer/Footer";
+
+describe("Footer", () => {
+    beforeEach(async () => {
+        await waitFor(() => {
+            render(
+                <AuthUserProvider>
+                    <BrowserRouter>
+                        <Footer />
+                    </BrowserRouter>
+                </AuthUserProvider>
+            );
+        });
+    });
+
+    it("should render the year", async () => {
+        const year = String(new Date().getFullYear());
+
+        await waitFor(() => {
+            const yearNow = screen.getByText(/Tous droits réservés/i);
+            expect(yearNow).toBeInTheDocument();
+            expect(yearNow).toHaveTextContent(`${year}`);
+        });
+    });
+});
